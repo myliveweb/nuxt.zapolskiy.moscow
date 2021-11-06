6a8c630016c7bcb03699ca16516b0f5f6e95d08b
Sergey <myliveweb@mail.ru>
2021-11-06 18:58:44 +0300

    Add React

--- components/FirstScreen.vue
+++ components/FirstScreen.vue
@@ -4,9 +4,10 @@
     <div class="grid">
       <div class="item item-1">
         <div class="link jquery"><a href="https://zapolskiy.moscow/">Laravel (Core JQuery)</a></div>
-        <div class="link nextjs"><a href="https://next.zapolskiy.moscow/">React.js (Core Next.js SSR)</a></div>
-        <div class="link angular"><a href="https://angular.zapolskiy.moscow/">Angular (Core Angular 12)</a></div>
+        <div class="link react"><a href="https://react.zapolskiy.moscow/">React.js (Core React.js)</a></div>
         <div class="nuxt"><span>Vue.js (Core Nuxt.js SSR)</span></div>
+        <div class="link angular"><a href="https://angular.zapolskiy.moscow/">Angular (Core Angular 12)</a></div>
+        <div class="link nextjs"><a href="https://next.zapolskiy.moscow/">React.js (Core Next.js SSR)</a></div>
       </div>
       <div class="item item-2">
         <div>Тел: <b>+7 962 206-4142</b></div>
